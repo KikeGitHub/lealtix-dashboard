28c74815b538ccbd009891c505d99eaae9dfa1a0
KikeGitHub <kike2013edj@gmail.com>
2025-10-07 22:08:39 -0600

    first commit

---
+++ src/app/layout/component/app.menu.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { AppMenuitem } from './app.menuitem';
+
+@Component({
+    selector: 'app-menu',
+    standalone: true,
+    imports: [CommonModule, AppMenuitem, RouterModule],
+    template: `<ul class="layout-menu">
+        <ng-container *ngFor="let item of model; let i = index">
+            <li app-menuitem *ngIf="!item.separator" [item]="item" [index]="i" [root]="true"></li>
+            <li *ngIf="item.separator" class="menu-separator"></li>
+        </ng-container>
+    </ul> `
+})
+export class AppMenu {
+    model: MenuItem[] = [];
+
+    ngOnInit() {
+        this.model = [
+            {
+                label: 'Home',
+                items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/'] },
+                        { label: 'Admin Page', icon: 'pi pi-fw pi-globe',  routerLink: ['/landing']},
+                        { label: 'Campañas', icon: 'pi pi-fw pi-id-card', routerLink: ['/uikit/formlayout']},
+                        { label: 'Reportes', icon: 'pi pi-fw pi-chart-bar', routerLink: ['/uikit/charts'] },
+                        { label: 'Usuarios', icon: 'pi pi-fw pi-id-card', routerLink: ['/uikit/formlayout'] },
+                        { label: 'Utils', icon: 'pi pi-fw pi-table', routerLink: ['/uikit/table'] }
+                    ]
+            }
+        ];
+    }
+}
